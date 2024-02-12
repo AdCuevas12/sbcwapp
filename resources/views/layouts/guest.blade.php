8002f97447ffc6237c48cffba2e7ce71d063db1a
AdCuevas12 <ralphkhenadrian.c@gmail.com>
2024-02-12 20:12:02 +0800

    reroll

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,7 @@
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/logo.png') }}">
 
         <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
